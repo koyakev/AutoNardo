4b49e726b4a3520f7096d2431b1fa32ead6a4731
HoshikawaShiori <jprofeta000@gmail.com>
2025-05-12 15:24:10 +0800

    Refactor UserController for improved readability and structure. Updated method formatting and added user session check in car_view method to redirect unauthenticated users to the login page.

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,32 +1,44 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class UserController extends CI_Controller {
-    public function __construct() {
-        parent::__construct();
-        $this->load->model('User_model');
-        $this->load->model('Car_model');
-    }
+class UserController extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('User_model');
+		$this->load->model('Car_model');
+	}
 
-    public function index() {
-        $data['cars'] = $this->Car_model->get_cars()['cars'];
+	public function index()
+	{
+		$data['cars'] = $this->Car_model->get_cars()['cars'];
 
-        $this->load->view('user/landing', $data);
-    }
+		$this->load->view('user/landing', $data);
+	}
 
-    public function login() {
-        $this->load->view('user/login');
-    }
+	public function login()
+	{
+		$this->load->view('user/login');
+	}
 
-	public function register() {
+	public function register()
+	{
 		$this->load->view('user/register');
 	}
 
-    public function car_view($id) {
-        $data['car'] = $this->Car_model->get_car($id);
-		$this->load->view('user/header', $data);
-		$this->load->view('user/nav', $data);
-        $this->load->view('user/car_view', $data);
-		$this->load->view('user/footer', $data);
-    }
+	public function car_view($id)
+	{
+		if ($this->session->userdata('user') && $this->session->userdata('isAdmin') !== 1) {
+			$car = $this->Car_model->get_car($id);
+			$data['title'] = "Autonardo | Booking | " . $car['make'] . " " . $car['model'];
+			$data['car'] = $this->Car_model->get_car($id);
+			$this->load->view('user/header', $data);
+			$this->load->view('user/nav', $data);
+			$this->load->view('user/car_view', $data);
+			$this->load->view('user/footer', $data);
+		} else {
+			redirect('user/login');
+		}
+	}
 }
