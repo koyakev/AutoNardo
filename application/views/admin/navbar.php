8187e055cf3e128e27556aabea62240e0a20a8dc
koyakev <kevinyu2201@gmail.com>
2025-05-17 00:26:17 +0800

    fahavol

--- application/views/admin/navbar.php
+++ application/views/admin/navbar.php
@@ -3,7 +3,7 @@
         <li class="nav-link mx-3"><a class="text-white text-decoration-none" href="<?= site_url('admin/') ?>">Home</a></li>
         <li class="nav-link mx-3"><a class="text-white text-decoration-none" href="<?= site_url('admin/users_list') ?>">Users</a></li>
         <li class="nav-link mx-3"><a class="text-white text-decoration-none" href="<?= site_url('admin/cars_list') ?>">Cars</a></li>
-        <li class="nav-link mx-3"><a class="text-white text-decoration-none" href="<?= site_url('admin/sales_view') ?>">Sales</a></li>
+        <li class="nav-link mx-3"><a class="text-white text-decoration-none" href="<?= site_url('admin/sales_view') ?>">Bookings</a></li>
         <li class="nav-link mx-3 ms-auto"><a class="text-white text-decoration-none" href="<?= site_url('auth/logout') ?>">Logout</a></li>
     </ul>
 </nav>
