01676c87378741f0b4b1a80058ddca88122bffeb
HoshikawaShiori <jprofeta000@gmail.com>
2025-05-16 20:49:14 +0800

    Enhance car display layout on landing page
    - Updated the car card structure to use a flexible layout with a ratio for images.
    - Improved the card body layout to ensure the "Book Now!" button is positioned at the bottom.
    - Added object-fit styling to images for better visual consistency.

--- application/views/user/landing.php
+++ application/views/user/landing.php
@@ -1,17 +1,20 @@
-
 <section class="py-5">
     <div class="container">
         <h1 class="mb-4">Available Cars</h1>
 
         <div class="row g-4">
             <?php foreach($cars as $car): ?>
-                <div class="col-md-6 col-lg-4">
-                    <div class="card h-100">
-                        <img src="<?= base_url('uploads/' . $car['image']) ?>" class="card-img-top" alt="<?= $car['model'] ?>">
-                        <div class="card-body">
+                <div class="col-md-6 col-lg-4 d-flex">
+                    <div class="card h-100 w-100 d-flex flex-column">
+                        <div class="ratio ratio-4x3">
+                            <img src="<?= base_url('uploads/' . $car['image']) ?>" class="card-img-top object-fit-cover" alt="<?= $car['model'] ?>">
+                        </div>
+                        <div class="card-body d-flex flex-column">
                             <h5 class="card-title"><?= $car['model'] ?> <?= $car['make'] ?></h5>
                             <p class="card-text mb-3">₱<?= number_format($car['rental_price_per_day'], 0) ?>/day</p>
-                            <a href="<?= site_url('/car_view/' . $car['id']) ?>" class="btn btn-primary w-100">Book Now!</a>
+                            <div class="mt-auto">
+                                <a href="<?= site_url('/car_view/' . $car['id']) ?>" class="btn btn-primary w-100">Book Now!</a>
+                            </div>
                         </div>
                     </div>
                 </div>
